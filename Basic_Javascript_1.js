ffd45fd3cebba7907c6e5564edb15f9c0a657f35
ysaechao30 <ysaechao30@gmail.com>
2020-01-22 21:50:12 -0800

    Initial commit

---
+++ Basic_Javascript_1.js
@@ -0,0 +1,12 @@
+alert("Hello, World! I am an alert box!");
+
+
+
+<script>
+		function My_First_Function() {
+			var String = "Kiss me, I'm Irish!";
+			car result = String.fontcolor("green");
+			document.getElementById("Irish").innerhtml=
+			result;
+	}
+</script>
